0f00146cf4555f0584f0480a6c90b2e4a86af16d
ginxx009 <macandili09@gmail.com>
2021-05-29 22:31:36 +0800

    added liff

--- src/screen/Task6/index.js
+++ src/screen/Task6/index.js
@@ -1,14 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import 'antd/dist/antd.css';
 import { Row, Col } from 'antd';
 import { Input, Button } from 'antd';
-import { PAY_BUTTON } from '../../utils/const';
+import { PAY_BUTTON, LIFF_APP_ID } from '../../utils/const';
+import liff from '@line/liff';
 
 const Task6 = () => {
+  const [liffReady, setLiffReady] = useState(false);
   const onSubmit = () => {
     console.log('Pay');
   };
+
+  const sendMessage = async () => {
+    liff
+      .sendMessages([
+        {
+          type: 'text',
+          text: `[${MESSAGE_FORMAT_TITLE}]\n${MESSAGE_FORMAT_VISIT_CODE}: ${
+            data.qrcode
+          }\n${MESSAGE_FORMAT_PAYMENT_METHOD}: ${data.payment.toUpperCase()}`
+        }
+      ])
+      .then(function() {
+        liff.closeWindow();
+      });
+  };
+
+  useEffect(() => {
+    const initLiff = () => {
+      liff
+        .init({
+          liffId: LIFF_APP_ID
+        })
+        .then(function() {
+          console.log('Liff is ready');
+          setLiffReady(true);
+        })
+        .catch(error => {
+          console.log('Liff error: ', error);
+          setLiffReady(false);
+        });
+    };
+
+    initLiff();
+  }, []);
 
   return (
     <div className="content">
